85ae3cff4e6a9225411720ef68c9867c59b77ef7
Pelayo <pmartinezcapela@gmail.com>
2022-12-12 18:13:06 +0100

    añadidas pags

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,34 @@
-import { Outlet, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
 
 function Header() {
   return (
     <div className="header">
-      <nav>
-        <ul>
-          <li>
-            <Link to={`about`}>About</Link>
-          </li>
-        </ul>
+      <nav className="header-content">
+        <Button variant="text" component={Link} to={""}>
+          Main
+        </Button>
+        <Button variant="text" component={Link} to={"about"}>
+          About
+        </Button>
+        <Button variant="text" component={Link} to={"studies"}>
+          Studies
+        </Button>
+        <Button variant="text" component={Link} to={"work"}>
+          Work
+        </Button>
+        <Button variant="text" component={Link} to={"contact"}>
+          Contact
+        </Button>
+        <select
+          class="idioma form-select form-select-sm ms-auto me-3"
+          aria-label="Default select example"
+        >
+          <option selected value="esp">
+            🇪🇸
+          </option>
+          <option value="ing">🇬🇧</option>
+        </select>
       </nav>
     </div>
   );
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,9 @@ import App from "./App";
 
 import Main from "./components/Main";
 import About from "./pages/About";
+import Studies from "./pages/Studies";
+import Work from "./pages/Work";
+import Contact from "./pages/Contact";
 import Error from "./pages/Error";
 
 export const routes = [
@@ -17,6 +20,18 @@ export const routes = [
       {
         path: "about",
         element: <About />,
+      },
+      {
+        path: "studies",
+        element: <Studies />,
+      },
+      {
+        path: "work",
+        element: <Work />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
       },
     ],
   },
